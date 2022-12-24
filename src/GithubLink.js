8517ed5604ac8a52afe38de47d8c146f44a6994e
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2022-12-24 19:44:28 +0100

    add

---
+++ src/GithubLink.js
@@ -0,0 +1,16 @@
+import "./GithubLink.css";
+
+export default function GithubLink() {
+  return (
+    <div className="GithubLink">
+      <button class="github">
+        <a
+          href="https://github.com/NataliiaBoiko22/weather-react"
+          target="_blank"
+          rel="noreferrer">
+          Github
+        </a>
+      </button>
+    </div>
+  );
+}
